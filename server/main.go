4b1d834506dd790d7ad8a37168f35899222f4515
Jake Casaletto <jake.m.casaletto@gmail.com>
2018-02-18 22:44:00 -0800

    add react-based frontend

--- server/main.go
+++ server/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
 func main() {
-	fmt.Println("Power up!")
+	http.Handle("/", http.FileServer(http.Dir("")))
+	http.HandleFunc("/ping", ping)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
